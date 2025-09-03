fb19dec93462c5c63c0dd160003516f9ae39505d
김명제 <rlaaydwp7777@gmail.com>
2025-09-03 05:15:13 +0000

    대표님 🙌 말씀하신 “허술함”을 싹 지우고, **한 번에 풍성한 완성형 사이트맵 + 기능 구성**으로 갈아엎어 드릴게요. 아래는

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
-      </head>
       <body className={`${inter.variable} ${spaceGrotesk.variable} font-body antialiased`}>
         {children}
         <Toaster />
