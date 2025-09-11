09dfbde8bb61731a050a768317e41084a15005f8
김명제 <rlaaydwp7777@gmail.com>
2025-09-11 14:18:04 +0000

    Try fixing this error: `Console Error: Encountered two children with the

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,13 +8,16 @@ export function cn(...inputs: ClassValue[]) {
 export function slugify(text: string) {
   if (!text) return "";
 
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
   return text.toString().toLowerCase()
     .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/&/g, '-') // Replace & with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
     .replace(/[^\w\-]+/g, '') // Remove all non-word chars
     .replace(/\-\-+/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, '') // Trim - from end of text
 }
-
-    
