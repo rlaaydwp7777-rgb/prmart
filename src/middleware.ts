4581089e40711995b69ecf6309cfe7682a4d858f
김명제 <rlaaydwp7777@gmail.com>
2025-09-28 06:57:03 +0000

    Try fixing this error: `Runtime Error: Error: The edge runtime does not

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 // src/middleware.ts
+export const runtime = 'nodejs';
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { adminAppInstance } from "@/lib/firebaseAdmin";
